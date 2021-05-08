46135af6919d8a7ad27318e83bf1c0c8d0134411
Haylee <17357481+hailstormCodes@users.noreply.github.com>
2021-05-08 17:28:32 -0400

    Added Logo, Input field, Particles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import Particles from 'react-particles-js';
+
 import Navigation from "./components/navigation/navigation.component";
+import Logo from './components/logo/logo.component';
+import Rank from './components/rank/rank.component';
+import ImageLinkForm from './components/image-link-form/image-link-form.component';
+
 import "./App.css";
+
+const particleOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true, 
+          value_area: 500
+        }
+      }
+    }
+  }
+
 
 function App() {
   return (
     <div className='App'>
+    <Particles 
+    className='particles'
+    params={particleOptions}
+    style={{
+      width: '100%',
+    }}
+  />
       <Navigation />
-      {/*<Logo />
-          <ImageLinkForm />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+          {/*
           <FaceRecognition /> */}
     </div>
   );
---
+++ src/components/rank/rank.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Rank = () => {
+  return (
+    <div className='ma4 mt0'>
+      <div classname='white f3'>
+      {'Haylee your current rank is'}
+      </div>
+      <div classname='white f1'>
+      {'#5'}
+      </div>
+      </div>
+  );
+}
+export default Rank;
